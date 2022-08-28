7d74ff2ca3b6aca7dd8b32b0ec1b31c2621109a3
TweakPY <69265406+Filza2@users.noreply.github.com>
2022-08-28 03:28:50 +0300

    Update PRX-Down.py

--- PRX-Down.py
+++ PRX-Down.py
@@ -1,11 +1,14 @@
-import requests,time
+from requests import get
 print("""
-╔═╗ ╦═╗ ═╗ ╦  ╔═╗┌─┐┬─┐┌─┐┌─┐┌─┐
-╠═╝ ╠╦╝ ╔╩╦╝  ╚═╗│  ├┬┘├─┤├─┘├┤ 
-╩   ╩╚═ ╩ ╚═  ╚═╝└─┘┴└─┴ ┴┴  └─┘
-By @TweakPY - @vv1ck
-""")
-print('____________________')                
+██████╗ ██████╗ ██╗  ██╗     ███████╗ ██████╗
+██╔══██╗██╔══██╗╚██╗██╔╝     ██╔════╝██╔════╝
+██████╔╝██████╔╝ ╚███╔╝█████╗███████╗██║     
+██╔═══╝ ██╔══██╗ ██╔██╗╚════╝╚════██║██║     
+██║     ██║  ██║██╔╝ ██╗     ███████║╚██████╗
+╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝     ╚══════╝ ╚═════╝
+
+           By @TweakPY - @vv1ck
+____________________""")             
 p=int(input("Proxy Type\n\n[1]-http   [2]-https\n[3]-socks4 [4]-socks5\n> "))
 print('____________________')
 if p==1:proxy_type="http"
@@ -20,11 +23,9 @@ elif ssl==3:ssl_type="all"
 time_out=int(input("TimeOut\n\n[50-10000]\n>"))
 print('____________________')
 print(f"[!] Auto country:[all] Protocol:[{proxy_type}] SSL:[{ssl_type}] Time Out:[{time_out}]")
-time.sleep(0.5)
 print('____________________')
-req=requests.get(f'https://api.proxyscrape.com/v2/?request=getproxies&protocol={proxy_type}&timeout={time_out}&country=all&ssl={ssl_type}&anonymity=all').text
+req=get(f'https://api.proxyscrape.com/v2/?request=getproxies&protocol={proxy_type}&timeout={time_out}&country=all&ssl={ssl_type}&anonymity=all').text
 with open('proxy.txt','w') as Proxy:
 	Proxy.write(req)
 	print('[+] Done')
-	print('[\/] Proxy List Added To File : "proxy.txt"')
-
+	print('[+] Proxy List Added To File : "proxy.txt"')
